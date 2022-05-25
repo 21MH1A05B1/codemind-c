66df41eccebbe68380f1b70f820b68e7cb798c19
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-05-25 22:26:31 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<n+1;i++)     {         for(j=1;j<n+1;j++)         if(i==j || i==n+1-j)         {             printf("%d ",n-i+1);         }         else         {             printf(" ",n-i+1);         }         printf(" ");     } }

---
+++ Print_the_following_pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<n+1;i++)
+    {
+        for(j=1;j<n+1;j++)
+        if(i==j || i==n+1-j)
+        {
+            printf("%d ",n-i+1);
+        }
+        else
+        {
+            printf(" ",n-i+1);
+        }
+        printf("
+");
+    }
+}
