b9b656f05cf9fe63a6a943f37cccec8b16d5854e
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-15 15:35:55 +0530

    #include<stdio.h> int main() {     int i,j,m,n,s=0,max=0;     scanf("%d%d",&m,&n);     int a[m][n];     for(i=0;i<m;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&a[i][j]);         }     }     for(j=0;j<n;j++)     {         s=0;         for(i=0;i<m;i++)         {             s+=a[i][j];         }         if(s>max)         {             max=s;         }           }         printf("%d",max); }

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,m,n,s=0,max=0;
+    scanf("%d%d",&m,&n);
+    int a[m][n];
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(j=0;j<n;j++)
+    {
+        s=0;
+        for(i=0;i<m;i++)
+        {
+            s+=a[i][j];
+        }
+        if(s>max)
+        {
+            max=s;
+        }
+          }
+        printf("%d",max);
+}
