eb2bd037506173834b7ea6a71005e60d6edd5817
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-18 20:51:28 +0530

    #include<stdio.h> int main() {     int i,j,s=0,m,n,max=0;     scanf("%d %d",&m,&n);     int a[m][n];     for(i=0;i<m;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&a[i][j]);         }     }     for(j=0;j<n;j++)     {         s=0;         for(i=0;i<m;i++)         {                 s+=a[i][j];         }         printf("%d ",s);     } }

--- Column_wise_sum_of_a_matrix.c
+++ Column_wise_sum_of_a_matrix.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,s=0,m,n;
-    scanf("%d%d",&m,&n);
+    int i,j,s=0,m,n,max=0;
+    scanf("%d %d",&m,&n);
     int a[m][n];
     for(i=0;i<m;i++)
     {
@@ -16,7 +16,7 @@ int main()
         s=0;
         for(i=0;i<m;i++)
         {
-            s+=a[i][j];
+                s+=a[i][j];
         }
         printf("%d ",s);
     }
