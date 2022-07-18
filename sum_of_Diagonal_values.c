dca74e25e1c95b82a58e5fba9e95712daa6dbe3d
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-18 20:28:36 +0530

    #include<stdio.h> int main() {     int i,j,s=0,m,n;     scanf("%d%d",&m,&n);     int a[m][n];     for(i=0;i<m;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<m;i++)     {         for(j=0;j<n;j++)         {             if(i==j || i+j==(m-1))             {                 s+=a[i][j];             }         }     }     printf("%d",s); }

--- sum_of_Diagonal_values.c
+++ sum_of_Diagonal_values.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,s1=0,s2=0,m,n;
+    int i,j,s=0,m,n;
     scanf("%d%d",&m,&n);
     int a[m][n];
     for(i=0;i<m;i++)
@@ -15,15 +15,11 @@ int main()
     {
         for(j=0;j<n;j++)
         {
-            if(i==j)
+            if(i==j || i+j==(m-1))
             {
-                 s1+=a[i][j];
-            }
-            if(i!=j && i+j==m-1)
-            {
-                s2+=a[i][j];
+                s+=a[i][j];
             }
         }
     }
-     printf("%d",s1+s2);
+    printf("%d",s);
 }
