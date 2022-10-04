ed81410522d3912f65b458a045b9f488adb4b919
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-10-04 22:33:31 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,deci;     scanf("%d",&n);     int arr[n],i,j=n-1;     for(i=0;i<n;i++)     {        scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         deci+=arr[i]*pow(2,j);         j--;     }     printf("%d",deci); }

--- Binary_Array_to_decimal.c
+++ Binary_Array_to_decimal.c
@@ -2,17 +2,17 @@
 #include<math.h>
 int main()
 {
-    int n,dec;
+    int n,deci;
     scanf("%d",&n);
     int arr[n],i,j=n-1;
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+       scanf("%d",&arr[i]); 
     }
     for(i=0;i<n;i++)
     {
-        dec+=arr[i]*pow(2,j);
+        deci+=arr[i]*pow(2,j);
         j--;
     }
-    printf("%d",dec);
+    printf("%d",deci);
 }
